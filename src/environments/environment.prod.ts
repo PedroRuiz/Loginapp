1ee84bd922552b35b778f7976ad638ba2fe78d22
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-06-04 21:12:53 +0200

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDzIBQwa4fR6Ink3rPNP3rgO5U6nGYJIjw',
+    authDomain: 'loginapp-cd2c8.firebaseapp.com',
+    databaseURL: 'https://loginapp-cd2c8.firebaseio.com',
+    projectId: 'loginapp-cd2c8',
+    storageBucket: '',
+    messagingSenderId: '105148222148'
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDzIBQwa4fR6Ink3rPNP3rgO5U6nGYJIjw',
+    authDomain: 'loginapp-cd2c8.firebaseapp.com',
+    databaseURL: 'https://loginapp-cd2c8.firebaseio.com',
+    projectId: 'loginapp-cd2c8',
+    storageBucket: '',
+    messagingSenderId: '105148222148'
+
+  }
 };
 
 /*
