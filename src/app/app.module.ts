9a94609038f08f79ab97045f8e110383ae43a18b
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-06-04 16:47:56 +0200

    Creado componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './compomentes/home-page/home-page.component';
+import { NavbarComponent } from './compomentes/navbar/navbar.component';
+import { RegisterPageComponent } from './compomentes/register-page/register-page.component';
+import { LoginPageComponent } from './compomentes/login-page/login-page.component';
+import { PrivadoPageComponent } from './compomentes/privado-page/privado-page.component';
+import { NotFoundPageComponent } from './compomentes/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivadoPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
